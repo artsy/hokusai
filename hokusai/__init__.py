278ed26320dac15350b84e68005c2e3f982a9fa9
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-01-26 13:50:49 -0500

    merge stack_up and stack_down into stack command with up / down subcommands and add status subcommand

--- hokusai/__init__.py
+++ hokusai/__init__.py
@@ -263,6 +263,26 @@ def stack_down(context):
   print_green("Stack %s deleted" % kubernetes_yml)
   return 0
 
+def stack_status(context):
+  config = HokusaiConfig().check()
+  kubernetes_yml = os.path.join(os.getcwd(), "hokusai/%s.yml" % context)
+  if not os.path.isfile(kubernetes_yml):
+    print_red("Yaml file %s does not exist for given context." % kubernetes_yml)
+    return -1
+
+  try:
+    switch_context_result = check_output("kubectl config use-context %s" % context, stderr=STDOUT, shell=True)
+    print_green("Switched context to %s" % context)
+    if 'no context exists' in switch_context_result:
+      print_red("Context %s does not exist.  Check ~/.kube/config" % context)
+      return -1
+    elif 'switched to context' in switch_context_result:
+      check_call("kubectl describe -f %s" % kubernetes_yml, shell=True)
+  except CalledProcessError:
+    print_red('Stack status failed')
+    return -1
+  return 0
+
 def deploy(context, tag):
   config = HokusaiConfig().check()
 
