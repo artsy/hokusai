3d668dd0b46ecbaf709cd425371fec8ce4cdf89b
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-02-15 17:02:06 -0500

    pretty print images both local and remote

--- hokusai/commands/images.py
+++ hokusai/commands/images.py
@@ -1,11 +1,21 @@
 from subprocess import check_call, CalledProcessError
 
 from hokusai.config import HokusaiConfig
+from hokusai.common import *
 
 def images():
   config = HokusaiConfig().check()
   try:
+    print('\n')
+    print_green('REMOTE IMAGES')
+    print_green('---------------------------')
     check_call("docker images %s" % config.aws_ecr_registry, shell=True)
+    print('\n')
+    print_green('LOCAL IMAGES')
+    print_green('---------------------------')
+    check_call("docker images build_%s" % config.project_name, shell=True)
+    check_call("docker images ci_%s" % config.project_name, shell=True)
+    print('\n')
   except CalledProcessError:
     return -1
   return 0
