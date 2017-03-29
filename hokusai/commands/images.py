47a26122eaf2a3b66f53553b59d7afa970efa827
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-03-29 10:54:31 -0400

    images command lists only remote

--- hokusai/commands/images.py
+++ hokusai/commands/images.py
@@ -1,3 +1,7 @@
+from operator import itemgetter
+
+import boto3
+
 from hokusai.command import command
 from hokusai.config import HokusaiConfig
 from hokusai.common import print_green, shout
@@ -5,13 +9,22 @@ from hokusai.common import print_green, shout
 @command
 def images():
   config = HokusaiConfig().check()
-  print('\n')
-  print_green('REMOTE IMAGES')
-  print_green('---------------------------')
-  shout("docker images %s" % config.aws_ecr_registry, print_output=True)
-  print('\n')
-  print_green('LOCAL IMAGES')
-  print_green('---------------------------')
-  shout("docker images build_%s" % config.project_name, print_output=True)
-  shout("docker images ci_%s" % config.project_name, print_output=True)
-  print('\n')
+  client = boto3.client('ecr')
+
+  images = []
+  res = client.describe_images(registryId=config.aws_account_id,
+                                repositoryName=config.project_name)
+  images += res['imageDetails']
+
+  while 'nextToken' in res:
+    res = client.describe_images(registryId=config.aws_account_id,
+                                  repositoryName=config.project_name,
+                                  nextToken=res['nextToken'])
+    images += res['imageDetails']
+
+  print_green('Image Pushed At           | Image Tags')
+  print_green('-----------------------------------------------------------')
+  for image in sorted(images, key=itemgetter('imagePushedAt'), reverse=True):
+    if 'imageTags' not in image.keys():
+      continue
+    print("%s | %s" % (image['imagePushedAt'], ', '.join(image['imageTags'])))
