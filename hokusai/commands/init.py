095cd20e7b6b65c784058424134ca78512ec4400
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-03-29 16:06:21 -0400

    refactor init command - simplify options, namespace services, add secret refs and depends_on

--- hokusai/commands/init.py
+++ hokusai/commands/init.py
@@ -13,9 +13,8 @@ from hokusai.config import HokusaiConfig
 from hokusai.common import print_green, build_service, build_deployment, YAML_HEADER
 
 @command
-def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
-          run_command, development_command, test_command, port, target_port,
-            with_memcached, with_redis, with_mongo, with_postgres, with_rabbitmq):
+def init(project_name, aws_account_id, aws_ecr_region, framework, port,
+          with_memcached, with_redis, with_mongodb, with_postgres, with_rabbitmq):
 
   mkpath(os.path.join(os.getcwd(), 'hokusai'))
 
@@ -23,12 +22,10 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
 
   if framework == 'rack':
     dockerfile = env.get_template("Dockerfile-ruby.j2")
-    if run_command is None:
-      run_command = 'bundle exec foreman start'
-    if development_command is None:
-      development_command = 'bundle exec foreman start'
-    if test_command is None:
-      test_command = 'bundle exec rake'
+    base_image = 'ruby:latest'
+    run_command = 'bundle exec rackup'
+    development_command = 'bundle exec rackup'
+    test_command = 'bundle exec rake'
     runtime_environment = {
       'development': ["RACK_ENV=development"],
       'test': ["RACK_ENV=test"],
@@ -38,12 +35,10 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
 
   elif framework == 'nodejs':
     dockerfile = env.get_template("Dockerfile-node.j2")
-    if run_command is None:
-      run_command = 'node index.js'
-    if development_command is None:
-      development_command = 'node index.js'
-    if test_command is None:
-      test_command = 'npm test'
+    base_image = 'node:latest'
+    run_command = 'node index.js'
+    development_command = 'node index.js'
+    test_command = 'npm test'
     runtime_environment = {
       'development': ["NODE_ENV=development"],
       'test': ["NODE_ENV=test"],
@@ -53,12 +48,10 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
 
   elif framework == 'elixir':
     dockerfile = env.get_template("Dockerfile-elixir.j2")
-    if run_command is None:
-      run_command = 'mix run --no-halt'
-    if development_command is None:
-      development_command = 'mix run'
-    if test_command is None:
-      test_command = 'mix test'
+    base_image = 'elixir:latest'
+    run_command = 'mix run --no-halt'
+    development_command = 'mix run'
+    test_command = 'mix test'
     runtime_environment = {
       'development': ["MIX_ENV=dev"],
       'test': ["MIX_ENV=test"],
@@ -67,12 +60,14 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
     }
 
   with open(os.path.join(os.getcwd(), 'Dockerfile'), 'w') as f:
-    f.write(dockerfile.render(base_image=base_image, command=run_command, target_port=target_port))
+    f.write(dockerfile.render(base_image=base_image, command=run_command, target_port=port))
 
   with open(os.path.join(os.getcwd(), 'hokusai', "common.yml"), 'w') as f:
     services = {
       config.project_name: {
-        'build': '../'
+        'build': {
+          'context': '../'
+        }
       }
     }
     data = OrderedDict([
@@ -95,52 +90,60 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
 
       if compose_environment == 'development':
         services[config.project_name]['command'] = development_command
-        services[config.project_name]['ports'] = ["%s:%s" % (port, target_port)]
+        services[config.project_name]['ports'] = ["%s:%s" % (port, port)]
       if compose_environment == 'test':
         services[config.project_name]['command'] = test_command
 
       services[config.project_name]['environment'] = runtime_environment[compose_environment]
 
+      if with_memcached or with_redis or with_mongodb or with_postgres or with_rabbitmq:
+        services[config.project_name]['depends_on'] = []
+
       if with_memcached:
-        services['memcached'] = {
+        services["%s-memcached" % config.project_name] = {
           'image': 'memcached'
         }
         if compose_environment == 'development':
-          services['memcached']['ports'] = ["11211:11211"]
-        services[config.project_name]['environment'].append('MEMCACHED_SERVERS=memcached:11211')
+          services["%s-memcached" % config.project_name]['ports'] = ["11211:11211"]
+        services[config.project_name]['environment'].append("MEMCACHED_SERVERS=%s-memcached:11211" % config.project_name)
+        services[config.project_name]['depends_on'].append("%s-memcached" % config.project_name)
 
       if with_redis:
-        services['redis'] = {
+        services["%s-redis" % config.project_name] = {
           'image': 'redis:3.2-alpine'
         }
         if compose_environment == 'development':
-          services['redis']['ports'] = ["6379:6379"]
-        services[config.project_name]['environment'].append("REDIS_URL=redis://redis:6379/%d" % idx)
+          services["%s-redis" % config.project_name]['ports'] = ["6379:6379"]
+        services[config.project_name]['environment'].append("REDIS_URL=redis://%s-redis:6379/%d" % (config.project_name, idx))
+        services[config.project_name]['depends_on'].append("%s-redis" % config.project_name)
 
-      if with_mongo:
-        services['mongodb'] = {
+      if with_mongodb:
+        services["%s-mongodb" % config.project_name] = {
           'image': 'mongo:3.0',
           'command': 'mongod --smallfiles'
         }
         if compose_environment == 'development':
-          services['mongodb']['ports'] = ["27017:27017"]
-        services[config.project_name]['environment'].append("MONGO_URL=mongodb://mongodb:27017/%s" % compose_environment)
+          services["%s-mongodb" % config.project_name]['ports'] = ["27017:27017"]
+        services[config.project_name]['environment'].append("MONGO_URL=mongodb://%s-mongodb:27017/%s" % (config.project_name, compose_environment))
+        services[config.project_name]['depends_on'].append("%s-mongodb" % config.project_name)
 
       if with_postgres:
-        services['postgres'] = {
+        services["%s-postgres" % config.project_name] = {
           'image': 'postgres:9.4'
         }
         if compose_environment == 'development':
-          services['postgres']['ports'] = ["5432:5432"]
-        services[config.project_name]['environment'].append("DATABASE_URL=postgresql://postgres/%s" % compose_environment)
+          services["%s-postgres" % config.project_name]['ports'] = ["5432:5432"]
+        services[config.project_name]['environment'].append("DATABASE_URL=postgresql://%s-postgres/%s" % (config.project_name, compose_environment))
+        services[config.project_name]['depends_on'].append("%s-postgres" % config.project_name)
 
       if with_rabbitmq:
-        services['rabbitmq'] = {
+        services["%s-rabbitmq" % config.project_name] = {
           'image': 'rabbitmq:3.6-management'
         }
         if compose_environment == 'development':
-          services['rabbitmq']['ports'] = ["5672:5672","15672:15672"]
-        services[config.project_name]['environment'].append("RABBITMQ_URL=amqp://rabbitmq/%s" % compose_environment)
+          services["%s-rabbitmq" % config.project_name]['ports'] = ["5672:5672","15672:15672"]
+        services[config.project_name]['environment'].append("RABBITMQ_URL=amqp://%s-rabbitmq/%s" % (config.project_name, compose_environment))
+        services[config.project_name]['depends_on'].append("%s-rabbitmq" % config.project_name)
 
       data = OrderedDict([
         ('version', '2'),
@@ -154,21 +157,21 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
       environment = runtime_environment[stack]
 
       if with_memcached:
-        environment.append({'name': 'MEMCACHED_SERVERS', 'value': "%s-memcached:11211" % config.project_name})
+        environment.append({'name': 'MEMCACHED_SERVERS', 'valueFrom': {'secretKeyRef': { 'name': "%s-secrets" % config.project_name, 'key': 'MEMCACHED_SERVERS'}}})
       if with_redis:
-        environment.append({'name': 'REDIS_URL', 'value': "redis://%s-redis:6379/0" % config.project_name})
-      if with_mongo:
-        environment.append({'name': 'MONGO_URL', 'value': "mongodb://%s-mongodb:27017/%s" % (config.project_name, stack)})
+        environment.append({'name': 'REDIS_URL', 'valueFrom': {'secretKeyRef': { 'name': "%s-secrets" % config.project_name, 'key': 'REDIS_URL'}}})
+      if with_mongodb:
+        environment.append({'name': 'MONGO_URL', 'valueFrom': {'secretKeyRef': { 'name': "%s-secrets" % config.project_name, 'key': 'MONGO_URL'}}})
       if with_postgres:
-        environment.append({'name': 'DATABASE_URL', 'value': "postgresql://%s-postgres/%s" % (config.project_name, stack)})
+        environment.append({'name': 'DATABASE_URL', 'valueFrom': {'secretKeyRef': { 'name': "%s-secrets" % config.project_name, 'key': 'DATABASE_URL'}}})
       if with_rabbitmq:
-        environment.append({'name': 'RABBITMQ_URL', 'value': "amqp://%s-rabbitmq/%s" % (config.project_name, stack)})
+        environment.append({'name': 'RABBITMQ_URL', 'valueFrom': {'secretKeyRef': { 'name': "%s-secrets" % config.project_name, 'key': 'RABBITMQ_URL'}}})
 
       deployment_data = build_deployment(config.project_name,
                                           "%s:%s" % (config.aws_ecr_registry, stack),
-                                          target_port, environment=environment, always_pull=True)
+                                          port, environment=environment, always_pull=True)
 
-      service_data = build_service(config.project_name, port, target_port=target_port, internal=False)
+      service_data = build_service(config.project_name, port, target_port=port, internal=False)
 
       stack_yaml = deployment_data + service_data
 
@@ -180,7 +183,7 @@ def init(project_name, aws_account_id, aws_ecr_region, framework, base_image,
         stack_yaml += build_deployment("%s-redis" % config.project_name, 'redis:3.2-alpine', 6379)
         stack_yaml += build_service("%s-redis" % config.project_name, 6379)
 
-      if with_mongo:
+      if with_mongodb:
         stack_yaml += build_deployment("%s-mongodb" % config.project_name, 'mongodb:3.0', 27017)
         stack_yaml += build_service("%s-mongodb" % config.project_name, 27017)
 
