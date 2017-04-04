22a30d7a45b011107e5f845135a82b30aec9fd4c
izakp <isac.petruzzi@gmail.com>
2017-04-04 19:15:28 -0400

    fix indentation in test/utils.py

--- test/utils.py
+++ test/utils.py
@@ -20,22 +20,22 @@ def captured_output():
     sys.stdout, sys.stderr = old_out, old_err
 
 def mocked_subprocess(func=None, retval='mocked!', retcode=0):
-	def _decorate(function):
-		@wraps(function)
-		def wrapper(*args, **kwargs):
-		  _subprocess_call = common.call
-		  _subprocess_check_call = common.check_call
-		  _subprocess_check_output = common.check_output
-		  try:
-		    common.call = mock.create_autospec(common.call, return_value=retcode)
-		    common.check_call = mock.create_autospec(common.check_call, return_value=retcode)
-		    common.check_output = mock.create_autospec(common.check_output, return_value=retval)
-		    return function(*args, **kwargs)
-		  finally:
-		    common.call = _subprocess_call
-		    common.check_call = _subprocess_check_call
-		    common.check_output = _subprocess_check_output
-		return wrapper
-	if func:
-		return _decorate(func)
-	return _decorate
+  def _decorate(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+      _subprocess_call = common.call
+      _subprocess_check_call = common.check_call
+      _subprocess_check_output = common.check_output
+      try:
+        common.call = mock.create_autospec(common.call, return_value=retcode)
+        common.check_call = mock.create_autospec(common.check_call, return_value=retcode)
+        common.check_output = mock.create_autospec(common.check_output, return_value=retval)
+        return function(*args, **kwargs)
+      finally:
+        common.call = _subprocess_call
+        common.check_call = _subprocess_check_call
+        common.check_output = _subprocess_check_output
+    return wrapper
+  if func:
+    return _decorate(func)
+  return _decorate
