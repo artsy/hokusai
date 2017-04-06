18f9c7e7ac090186fdd0fb951bad3988782daf5c
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-04-06 14:44:43 -0400

    update dependencies args

--- hokusai/commands/dependencies.py
+++ hokusai/commands/dependencies.py
@@ -10,15 +10,15 @@ from hokusai.command import command
 from hokusai.common import print_red, print_green
 
 @command
-def dependencies(kubectl_version, platform, install_to, install_kubeconfig_to, bucket_name, key_name, bucket_region):
+def dependencies(kubectl_version, platform, install_to, install_config_to, bucket_name, key_name, bucket_region):
   print_green("Downloading and installing kubectl...")
   urllib.urlretrieve("https://storage.googleapis.com/kubernetes-release/release/v%s/bin/%s/amd64/kubectl" % (kubectl_version, platform), os.path.join('/tmp', 'kubectl'))
   os.chmod(os.path.join('/tmp', 'kubectl'), 0755)
   shutil.move(os.path.join('/tmp', 'kubectl'), os.path.join(install_to, 'kubectl'))
 
   print_green("Configuring kubectl...")
-  if not os.path.isdir(install_kubeconfig_to):
-    mkpath(install_kubeconfig_to)
+  if not os.path.isdir(install_config_to):
+    mkpath(install_config_to)
 
   bucket = boto3.resource('s3').Bucket(bucket_name)
-  bucket.download_file(key_name, os.path.join(install_kubeconfig_to, 'config'))
+  bucket.download_file(key_name, os.path.join(install_config_to, 'config'))
