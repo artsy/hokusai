5b4f2e3e96a537c7141c72606b23454d8af329d2
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-04-12 12:33:17 -0400

    finish ecr tests

--- test/unit/test_ecr.py
+++ test/unit/test_ecr.py
@@ -13,8 +13,32 @@ class TestECR(HokusaiUnitTestCase):
     self.ecr = ECR()
 
   @httpretty.activate
+  def test_registry(self):
+    httpretty.register_uri(httpretty.POST, "https://ecr.us-east-1.amazonaws.com/",
+                            body=open(os.path.join(os.getcwd(), 'test', 'fixtures', 'ecr-repositories-response.json')).read(),
+                            content_type="application/x-amz-json-1.1")
+    repositories = [str(repo['repositoryName']) for repo in self.ecr.registry()]
+    self.assertTrue('foo' in repositories)
+    self.assertTrue('bar' in repositories)
+    self.assertTrue('baz' in repositories)
+
+  @httpretty.activate
+  def test_project_repository_exists(self):
+    httpretty.register_uri(httpretty.POST, "https://ecr.us-east-1.amazonaws.com/",
+                            body=open(os.path.join(os.getcwd(), 'test', 'fixtures', 'ecr-repositories-response.json')).read(),
+                            content_type="application/x-amz-json-1.1")
+    self.assertTrue(self.ecr.project_repository_exists())
+
+  @httpretty.activate
+  def test_create_project_repository(self):
+    httpretty.register_uri(httpretty.POST, "https://ecr.us-east-1.amazonaws.com/",
+                            body=open(os.path.join(os.getcwd(), 'test', 'fixtures', 'ecr-repositories-response.json')).read(),
+                            content_type="application/x-amz-json-1.1")
+    self.assertTrue(self.ecr.create_project_repository())
+
+  @httpretty.activate
   def test_get_login(self):
     httpretty.register_uri(httpretty.POST, "https://ecr.us-east-1.amazonaws.com/",
-                           body='{"authorizationData":[{"authorizationToken":"QVdTOjc2VzhZRVVGSERTQUU5OERGREhTRlNERklVSFNEQUpLR0tTQURGR0tERg==","expiresAt":1E9,"proxyEndpoint":"https://12345.dkr.ecr.us-east-1.amazonaws.com"}]}',
+                           body=open(os.path.join(os.getcwd(), 'test', 'fixtures', 'ecr-authorization-response.json')).read(),
                            content_type="application/x-amz-json-1.1")
-    self.assertEqual(self.ecr.get_login(), 'docker login -u AWS -p 76W8YEUFHDSAE98DFDHSFSDFIUHSDAJKGKSADFGKDF -e none https://12345.dkr.ecr.us-east-1.amazonaws.com')
+    self.assertEqual(self.ecr.get_login(), 'docker login -u AWS -p 76W8YEUFHDSAE98DFDHSFSDFIUHSDAJKGKSADFGKDF -e none https://123456789012.dkr.ecr.us-east-1.amazonaws.com')
