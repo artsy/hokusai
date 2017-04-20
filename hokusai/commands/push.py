868560b6f6358d272c1525b6fce4eaa51e3df6b6
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-04-20 12:55:39 -0400

    support multiple tags in hokusai push

--- hokusai/commands/push.py
+++ hokusai/commands/push.py
@@ -6,7 +6,7 @@ from hokusai.ecr import ECR
 from hokusai.common import print_red, print_green, shout
 
 @command
-def push(tag):
+def push(tags):
   docker_compose_yml = os.path.join(os.getcwd(), 'hokusai/common.yml')
   shout("docker-compose -f %s -p build build" % docker_compose_yml, print_output=True)
   build = "build_%s:latest" % config.project_name
@@ -16,16 +16,16 @@ def push(tag):
     print_red("ECR repository %s does not exist... did you run `hokusai setup` for this project?" % config.project_name)
     return -1
 
-  if tag is None:
-    tag = shout('git rev-parse HEAD')
+  if len(tags) is 0:
+    tags = [shout('git rev-parse HEAD').strip()]
 
   shout(ecr.get_login())
 
-  shout("docker tag %s %s:%s" % (build, config.aws_ecr_registry, tag))
-  shout("docker push %s:%s" % (config.aws_ecr_registry, tag))
-  print_green("Pushed %s to %s:%s" % (build, config.aws_ecr_registry, tag))
+  for tag in tags:
+    shout("docker tag %s %s:%s" % (build, config.aws_ecr_registry, tag))
+    shout("docker push %s:%s" % (config.aws_ecr_registry, tag))
+    print_green("Pushed %s to %s:%s" % (build, config.aws_ecr_registry, tag))
 
   shout("docker tag %s %s:%s" % (build, config.aws_ecr_registry, 'latest'))
   shout("docker push %s:%s" % (config.aws_ecr_registry, 'latest'))
-  print_green("Updated tag %s:%s -> %s:%s" %
-      (config.aws_ecr_registry, tag, config.aws_ecr_registry, 'latest'))
+  print_green("Pushed %s to %s:%s" % (build, config.aws_ecr_registry, 'latest'))
