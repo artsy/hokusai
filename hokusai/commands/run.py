6786351f0f9edf44022d7fd7e9c4792f9b972962
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-06-13 11:43:11 +0200

    fix env vars passed to run command

--- hokusai/commands/run.py
+++ hokusai/commands/run.py
@@ -25,41 +25,40 @@ def run(context, command, tty, tag, env):
 
   name = "%s-hokusai-run-%s" % (config.project_name, uuid)
   image_name = "%s:%s" % (config.aws_ecr_registry, image_tag)
+  container = {
+    "args": command.split(' '),
+    "name": name,
+    "image": image_name,
+    "imagePullPolicy": "Always",
+    'envFrom': [{'configMapRef': {'name': "%s-environment" % config.project_name}}]
+  }
 
   if tty:
-    overrides = {
-      "apiVersion": "v1",
-      "spec": {
-        "containers": [
-          {
-            "args": command.split(' '),
-            "name": name,
-            "image": image_name,
-            "imagePullPolicy": "Always",
-            'envFrom': [{'configMapRef': {'name': "%s-environment" % config.project_name}}],
-            "stdin": True,
-            "stdinOnce": True,
-            "tty": True
-          }
-        ]
-      }
+    container.update({
+      "stdin": True,
+      "stdinOnce": True,
+      "tty": True
+    })
+
+  if len(env):
+    container['env'] = []
+    for s in env:
+      if '=' not in s:
+        print_red("Error: environment variables must be of the form 'KEY=VALUE'")
+        return -1
+      split = s.split('=', 1)
+      container['env'].append({'name': split[0], 'value': split[1]})
+
+  overrides = {
+    "apiVersion": "v1",
+    "spec": {
+      "containers": [container]
     }
+  }
+
+  if tty:
     shout(kctl.command("run %s -t -i --image=%s --restart=Never --overrides='%s' --rm" %
                    (name, image_name, json.dumps(overrides))), print_output=True)
   else:
-    overrides = {
-      "apiVersion": "v1",
-      "spec": {
-        "containers": [
-          {
-            "args": command.split(' '),
-            "name": name,
-            "image": image_name,
-            "imagePullPolicy": "Always",
-            'envFrom': [{'configMapRef': {'name': "%s-environment" % config.project_name}}]
-          }
-        ]
-      }
-    }
     return returncode(kctl.command("run %s --attach --image=%s --overrides='%s' --restart=Never --rm" %
                                       (name, image_name, json.dumps(overrides))))
