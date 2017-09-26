b68c74b636ebf81ead15e8128a348dff3ba521f6
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-09-26 19:53:54 +0200

    distribute script fixes

--- distribute.sh
+++ distribute.sh
@@ -2,18 +2,25 @@
 
 set -e
 
+# Tag release
+
 git fetch upstream --tags
 git tag v$(cat hokusai/VERSION.txt)
 git push upstream --tags
+
+# Pip package
+
 python setup.py sdist bdist_wheel
 twine upload dist/*
 rm -rf build/ dist/ hokusai.egg-info/
+
+# Docker image
 
 docker login
 
 docker build -t hokusai .
 
-docker tag hokusai:$(cat hokusai/VERSION.txt) artsy/hokusai:$(cat hokusai/VERSION.txt)
+docker tag hokusai:latest artsy/hokusai:$(cat hokusai/VERSION.txt)
 docker push artsy/hokusai:$(cat hokusai/VERSION.txt)
 
 docker tag hokusai:latest artsy/hokusai:latest
