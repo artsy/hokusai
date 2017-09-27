f67fe583397cb01d154bbd04a44b43400449d57e
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-09-27 20:51:23 +0200

    remove unreferenced service wrapper

--- hokusai/services/service.py
+++
@@ -1,8 +0,0 @@
-from hokusai.lib.config import config
-from hokusai.services.kubectl import Kubectl
-
-class Service(object):
-  def __init__(self, context):
-    self.context = context
-    self.kctl = Kubectl(self.context)
-    self.cache = self.kctl.get_object('service', selector="app=%s,layer=application" % config.project_name)
