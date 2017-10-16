5dab4d3b0ed4a927d66bac8c9111068dd7f312b4
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-10-16 15:42:43 +0200

    nest dev commands inside local

--- hokusai/cli/__init__.py
+++ hokusai/cli/__init__.py
@@ -1,4 +1,5 @@
 from hokusai.cli.base import base
 from hokusai.cli.local import local
+from hokusai.cli.local.dev import dev
 from hokusai.cli.remote import remote
 from hokusai.cli.remote.env import env
---
+++ hokusai/cli/local/dev.py
@@ -0,0 +1,64 @@
+import click
+
+import hokusai
+
+from hokusai.cli.local import local
+from hokusai.lib.common import set_verbosity, select_context, CONTEXT_SETTINGS
+
+@local.group()
+def dev(context_settings=CONTEXT_SETTINGS):
+  """Interact with docker-compose targeting the development stack defined in ./hokusai/development.yml"""
+  pass
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-b', '--build', type=click.BOOL, is_flag=True, help="Force rebuild the docker image before running")
+@click.option('-d', '--detach', type=click.BOOL, is_flag=True, help="Run containers in the background")
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+def start(build, detach, verbose):
+  """Start the development stack defined in ./hokusai/development.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_start(build, detach)
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+def stop(verbose):
+  """Stop the development stack defined in ./hokusai/development.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_stop()
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+def status(verbose):
+  """Print the status of the development stack defined in ./hokusai/development.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_status()
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+@click.option('-f', '--follow', type=click.BOOL, is_flag=True, help="Follow output")
+@click.option('-t', '--tail', type=click.INT, help="Number of lines of recent logs to display")
+def logs(follow, tail, verbose):
+  """Print logs from the development stack defined in ./hokusai/development.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_logs(follow, tail)
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('command')
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+def run(command, verbose):
+  """Run a command in the development stack's container with the name 'project-name' in hokusai/config.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_run(command)
+
+
+@dev.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
+def clean(verbose):
+  """Stop and remove all containers in the development stack defined in ./hokusai/development.yml"""
+  set_verbosity(verbose)
+  hokusai.dev_clean()
