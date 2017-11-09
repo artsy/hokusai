ab759d8f0715c03e7e09e3d856b8788d66458b6c
Isac Petruzzi <isac.petruzzi@gmail.com>
2017-11-09 13:56:49 +0100

    Add --stop option to dev command to clean up services

--- hokusai/cli/dev.py
+++ hokusai/cli/dev.py
@@ -2,10 +2,9 @@ import click
 
 import hokusai
 
-from hokusai.lib.config import config
-
 from hokusai.cli.base import base
 from hokusai.lib.common import set_verbosity, CONTEXT_SETTINGS
+from hokusai.lib.config import config
 
 @base.group()
 def dev(context_settings=CONTEXT_SETTINGS):
@@ -52,12 +51,15 @@ def logs(follow, tail, verbose):
 
 @dev.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('command')
-@click.option('--service-name', type=click.STRING, default=config.project_name, help="The service name to launch the container as (default: %s)" % config.project_name)
+@click.option('--service-name', type=click.STRING, help="The service name to launch the container as (default: the name 'project-name' in `hokusai/config.yml`)")
+@click.option('--stop', type=click.BOOL, is_flag=True, help='Stop all services after running the command')
 @click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
-def run(command, service_name, verbose):
+def run(command, service_name, stop, verbose):
   """Run a command in a new container in the development environment defined in ./hokusai/development.yml"""
   set_verbosity(verbose)
-  hokusai.dev_run(command, service_name)
+  if service_name is None:
+    service_name = config.project_name
+  hokusai.dev_run(command, service_name, stop)
 
 
 @dev.command(context_settings=CONTEXT_SETTINGS)
