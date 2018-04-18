cf766bc612754aff781419315f8c76bb11504cec
ashkan <ashkan@artsymail.com>
2018-04-18 17:20:08 -0400

    WIP

--- hokusai/cli/review_app.py
+++ hokusai/cli/review_app.py
@@ -1,6 +1,6 @@
 import click
-from shutil import copyfile, rmtree
-import fileinput
+from shutil import copyfile
+import yaml
 
 import hokusai
 
@@ -9,29 +9,22 @@ from hokusai.lib.common import set_verbosity, CONTEXT_SETTINGS
 from hokusai.lib.config import config
 
 @staging.group()
-def review_app(verbose):
+def review_app(context_settings=CONTEXT_SETTINGS):
   """Manage/Create review apps"""
-  set_verbosity(verbose)
-  hokusai.k8s_status(KUBE_CONTEXT)
+  pass
 
-@review_app.comand(context_settings=CONTEXT_SETTINGS)
-@click.argument('pr_number', type=click.STRING)
+@review_app.command(context_settings=CONTEXT_SETTINGS)
+@click.argument('app_name', type=click.STRING)
 @click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
-def create(pr_number, verbose):
+def create(app_name, verbose):
   set_verbosity(verbose)
   # copy staging yml file
-  pr_file_name = "hokusai/%s.yml" % pr_number
-  pr_project_name = "{0}_{1}".format(config.project_name, pr_number)
+  pr_file_name = "hokusai/%s.yml" % app_name
   copyfile('hokusai/staging.yml', pr_file_name)
-  # rename app name
-  for line in fileinput.input(pr_file_name, inplace=True):
-    print line.replace(config.project_name, pr_project_name)
-  hokusai.k8s_create(KUBE_CONTEXT, pr_number, pr_number)
-  # delete pr file
-  rmtree(pr_file_name)
 
-@review_app.comand(context_settings=CONTEXT_SETTINGS)
-@click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
-def list(verbose):
-  set_verbosity(verbose)
-  
+def change_namespace(original_yaml_file, new_namespace, current_namespace='default'):
+  with open(original_yaml_file, 'r') as stream:
+    try:
+      yaml_content = yaml.load(stream)
+    except yaml.YAMLError as exc:
+      print(exc)
