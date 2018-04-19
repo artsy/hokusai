026377db6f5958ef9b6b7f30b7e89ad256609ceb
ashkan18 <ashkan18@gmail.com>
2018-04-19 17:50:23 -0400

    Code review comments

--- hokusai/cli/review_app.py
+++ hokusai/cli/review_app.py
@@ -1,14 +1,17 @@
+from collections import OrderedDict
 import click
+
 from shutil import copyfile
 import yaml
 
 import hokusai
 
-from hokusai.cli.staging import staging, KUBE_CONTEXT
+from hokusai.cli.base import base
+from hokusai.cli.staging import KUBE_CONTEXT
 from hokusai.lib.common import set_verbosity, CONTEXT_SETTINGS
 from hokusai.lib.config import config
 
-@staging.group()
+@base.group()
 def review_app(context_settings=CONTEXT_SETTINGS):
   """Manage/Create review apps"""
   pass
@@ -16,20 +19,36 @@ def review_app(context_settings=CONTEXT_SETTINGS):
 @review_app.command(context_settings=CONTEXT_SETTINGS)
 @click.argument('app_name', type=click.STRING)
 @click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output')
-def create(app_name, verbose):
+@click.option('-sf', '--source-file', type=click.STRING, default='hokusai/staging.yml', help='Source deployment file')
+@click.option('-dn', '--destination-namespace', type=click.STRING, default=None, help='Target Namespace')
+def create(app_name, verbose, source_file, source_namespace, destination_namespace):
   set_verbosity(verbose)
   # copy staging yml file
-  create_new_app_yaml('hokusai/staging.yml', app_name)
+  if destination_namespace is None: destination_namespace = app_name
+  create_new_app_yaml(source_file, app_name, destination_namespace)
 
-def create_new_app_yaml(original_yaml_file, app_name, current_namespace='default'):
-  with open(original_yaml_file, 'r') as stream:
+def create_new_app_yaml(source_file, app_name, destination_namespace):
+  with open(source_file, 'r') as stream:
     try:
       yaml_content = list(yaml.load_all(stream))
-      # @TODO: replace namespace: default with new namespace name
+      # update namespace to destination namespace
+      for c in yaml_content: update_namespace(c, destination_namespace)
       # prepend new namespace definition to yaml
-      new_namespace = {"apiVersion": "v1", "kind": "Namespace", "meta": { "name": str(app_name) }}
+      new_namespace = OrderedDict([
+          ('apiVersion', 'v1'),
+          ('kind', 'Namespace'),
+          ('metadata', {
+            'name': str(app_name)
+          })
+        ])
       yaml_content = [new_namespace] + yaml_content
       with open("hokusai/%s.yml" % app_name, 'w') as output:
-        yaml.dump_all(yaml_content, output)
+        yaml.safe_dump_all(yaml_content, output, default_flow_style=False)
     except yaml.YAMLError as exc:
       print(exc)
+
+def update_namespace(yaml_section, destination_namespace):
+  if 'namespace' in yaml_section: yaml_section['namespace'] = destination_namespace
+  for k, v in yaml_section.iteritems():
+    if isinstance(v, dict):
+      update_namespace(v, destination_namespace)
