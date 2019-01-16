519da079b8ee1f0700b0f3e6d483099e8cc821f7
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-01-16 14:00:41 +0100

    more kubernetes tests

--- test/integration/test_kubernetes.py
+++ test/integration/test_kubernetes.py
@@ -11,20 +11,79 @@ from hokusai.lib.common import shout
 from hokusai.commands import kubernetes
 from hokusai.services.kubectl import Kubectl
 
-kubernetes.HOKUSAI_CONFIG_DIR = os.path.join(CWD, 'test/fixtures/project/hokusai')
 
 class TestKubernetes(HokusaiIntegrationTestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         kubernetes.ECR = MockECR
-    def tearDown(self):
-        shout(Kubectl(TEST_KUBE_CONTEXT).command("delete all --selector testFixture=true"), print_output=True)
+        kubernetes.HOKUSAI_CONFIG_DIR = os.path.join(CWD, 'test/fixtures/project/hokusai')
+        cls.kubernetes_yml = os.path.abspath(os.path.join(kubernetes.HOKUSAI_CONFIG_DIR, 'minikube.yml'))
+        cls.kctl = Kubectl(TEST_KUBE_CONTEXT)
+
+    @classmethod
+    def tearDownClass(cls):
+        with captured_output() as (out, err):
+            shout(cls.kctl.command("delete all --selector testFixture=true"), print_output=True)
 
     @patch('hokusai.lib.command.sys.exit')
-    def test_k8s_create(self, mocked_sys_exit):
-        with mock_verbosity(True):
-            # with captured_output() as (out, err):
+    def test_00_k8s_create(self, mocked_sys_exit):
+        with captured_output() as (out, err):
             kubernetes.k8s_create(TEST_KUBE_CONTEXT, yaml_file_name='minikube')
             mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('deployment.apps "hello-web" created', out.getvalue().strip())
+            self.assertIn('service "hello-web" created', out.getvalue().strip())
+            self.assertIn('Created Kubernetes environment %s' % self.__class__.kubernetes_yml,
+                            out.getvalue().strip())
 
-            kubernetes_yml = os.path.join(CWD, "%s/%s.yml" % (kubernetes.HOKUSAI_CONFIG_DIR, 'minikube'))
-            shout(Kubectl(TEST_KUBE_CONTEXT).command("get -f %s" % kubernetes_yml), print_output=True)
+    @patch('hokusai.lib.command.sys.exit')
+    def test_01_k8s_update(self, mocked_sys_exit):
+        with captured_output() as (out, err):
+            kubernetes.k8s_update(TEST_KUBE_CONTEXT, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('deployment.apps "hello-web" unchanged', out.getvalue().strip())
+            self.assertIn('service "hello-web" unchanged', out.getvalue().strip())
+            self.assertIn('Updated Kubernetes environment %s' % self.__class__.kubernetes_yml,
+                            out.getvalue().strip())
+
+    @patch('hokusai.lib.command.sys.exit')
+    def test_02_k8s_status(self, mocked_sys_exit):
+        with captured_output() as (out, err):
+            kubernetes.k8s_status(TEST_KUBE_CONTEXT, True, False, False, False, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('Resources', out.getvalue().strip())
+            self.assertIn('deployment.apps/hello-web', out.getvalue().strip())
+            self.assertIn('service/hello-web', out.getvalue().strip())
+            mocked_sys_exit.reset_mock()
+
+            kubernetes.k8s_status(TEST_KUBE_CONTEXT, True, False, True, False, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('Resources', out.getvalue().strip())
+            self.assertIn('Name:                   hello-web', out.getvalue().strip())
+            self.assertIn('Selector:               app=hello', out.getvalue().strip())
+            mocked_sys_exit.reset_mock()
+
+            kubernetes.k8s_status(TEST_KUBE_CONTEXT, False, True, False, False, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('Pods', out.getvalue().strip())
+            self.assertIn('hello-web', out.getvalue().strip())
+            self.assertTrue('ContainerCreating' in out.getvalue().strip() or 'Running' in out.getvalue().strip())
+            mocked_sys_exit.reset_mock()
+
+            kubernetes.k8s_status(TEST_KUBE_CONTEXT, False, True, True, False, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('Name:           hello-web', out.getvalue().strip())
+            mocked_sys_exit.reset_mock()
+
+            # TODO enable heapster in minikube to get top pods
+            # kubernetes.k8s_status(TEST_KUBE_CONTEXT, False, False, False, True, yaml_file_name='minikube')
+
+
+    @patch('hokusai.lib.command.sys.exit')
+    def test_03_k8s_delete(self, mocked_sys_exit):
+        with captured_output() as (out, err):
+            kubernetes.k8s_delete(TEST_KUBE_CONTEXT, yaml_file_name='minikube')
+            mocked_sys_exit.assert_called_once_with(0)
+            self.assertIn('deployment.apps "hello-web" deleted', out.getvalue().strip())
+            self.assertIn('service "hello-web" deleted', out.getvalue().strip())
+            self.assertIn('Deleted Kubernetes environment %s' % self.__class__.kubernetes_yml,
+                            out.getvalue().strip())
