45c245bc7bf48edc96dd9e582c2c03b15a09506c
Isac Petruzzi <isac.petruzzi@gmail.com>
2020-03-31 17:47:58 +0200

    refactor ConfigLoader

--- hokusai/lib/config_loader.py
+++ hokusai/lib/config_loader.py
@@ -1,38 +1,50 @@
 import os
 import tempfile
-import shutil
+from shutil import rmtree, copyfile
 
 from urlparse import urlparse
 
 import boto3
 import yaml
 
+from botocore.exceptions import ClientError
+
 from hokusai.lib.common import get_region_name
 from hokusai.lib.exceptions import HokusaiError
 
-class ConfigLoader
+class ConfigLoader:
   def __init__(self, uri):
     self.uri = uri
 
   def load(self):
     uri = urlparse(self.uri)
-    if not uri.path.endswith('yaml') or not uri.path.endswith('yml'):
+    if not uri.path.endswith('yaml') and not uri.path.endswith('yml'):
       raise HokusaiError('Uri must be of Yaml file type')
 
     tmpdir = tempfile.mkdtemp()
+    tmp_configfile = os.path.join(tmpdir, 'config')
 
-    switch(uri.scheme):
-      case 's3':
+    try:
+      if uri.scheme == 's3':
         client = boto3.client('s3', region_name=get_region_name())
-        tmp_configfile = os.path.join(tmpdir, 'config')
-        client.download_file(uri.netloc, uri.path.lstrip('/'), tmp_configfile)
+        try:
+          client.download_file(uri.netloc, uri.path.lstrip('/'), tmp_configfile)
+        except ClientError:
+          raise HokusaiError("Error fetching file %s" % self.uri)
+      else:
+        try:
+          copyfile(uri.path, tmp_configfile)
+        except IOError:
+          raise HokusaiError("Error copying file %s" % self.uri)
 
-      default:
-        tmp_configfile = uri.path
+      with open(tmp_configfile, 'r') as f:
+        struct = yaml.safe_load(f.read())
+        if type(struct) is not dict:
+          raise HokusaiError('Yaml is invalid')
+        return struct
 
-    with open(tmp_configfile, 'r') as f:
-      struct = yaml.safe_load(f.read())
-      if type(struct) is not obj:
-        raise HokusaiError('Yaml is invalid')
+    except HokusaiError:
+      raise
 
-      return struct
+    finally:
+      rmtree(tmpdir)
