5b1b4efdbd118828ccc55774826af663dac3af08
Isac Petruzzi <isac.petruzzi@gmail.com>
2020-04-08 19:01:56 +0200

    update template rendering tests

--- test/unit/test_template_renderer.py
+++ test/unit/test_template_renderer.py
@@ -15,11 +15,14 @@ class TestTemplateRenderer(HokusaiUnitTestCase):
 
   def test_template_renderer_renders_vars(self):
     template_config = {
-      'snippets': { 'containers': [ {'name': 'project-name-web'} ] },
-      'vars': { 'imageTag': 'spam' }
+      'vars': {
+        'containers': [{'name': 'project-name-web'}],
+        'imageTag': 'spam'
+        }
     }
     template_renderer = TemplateRenderer(self.template_path, template_config)
     k8s_payload = list(yaml.safe_load_all(template_renderer.render()))
+    self.assertEqual(k8s_payload[0]['spec']['template']['spec']['containers'][0]['name'], 'project-name-web')
     self.assertEqual(k8s_payload[0]['spec']['template']['spec']['containers'][0]['image'], 'spam')
 
   def test_template_renderer_errors_on_missing_variables(self):
