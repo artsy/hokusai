32177c5ccc46b7c10cac18b443d97bdc3e82ce70
Isac Petruzzi <isac.petruzzi@gmail.com>
2020-06-02 11:54:11 +0200

    refactor docker_compose_helpers.follow_extends to explicitly check for extends key

--- hokusai/lib/docker_compose_helpers.py
+++ hokusai/lib/docker_compose_helpers.py
@@ -11,15 +11,11 @@ from hokusai.services.yaml_spec import YamlSpec
 def follow_extends(docker_compose_yml):
   with open(docker_compose_yml, 'r') as f:
     rendered_templates = []
-    try:
-      struct = yaml.safe_load(f.read())
-      for service_name, service_spec in struct['services'].iteritems():
-        extended_filename = service_spec['extends']['file']
-        extended_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, extended_filename))
-        rendered_templates.append(YamlSpec(extended_template).to_file())
-    except Exception, err:
-      if isinstance(err, HokusaiError):
-        raise
-      else:
-        pass
+    struct = yaml.safe_load(f.read())
+    for service_name, service_spec in struct['services'].iteritems():
+      if 'extends' not in service_spec or 'file' not in service_spec['extends']:
+        continue
+      extended_filename = service_spec['extends']['file']
+      extended_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, extended_filename))
+      rendered_templates.append(YamlSpec(extended_template).to_file())
     return rendered_templates
