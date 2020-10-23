53d6b6579bec79204a7ede78b8d1ffae92087e1b
Isac Petruzzi <isac.petruzzi@gmail.com>
2020-10-23 11:41:32 +0200

    fix: use a NamedTemporaryFile for rendering Yaml specs

--- hokusai/services/yaml_spec.py
+++ hokusai/services/yaml_spec.py
@@ -1,5 +1,6 @@
 import os
 import atexit
+from tempfile import NamedTemporaryFile
 
 import yaml
 
@@ -44,7 +45,7 @@ class YamlSpec(object):
     file_basename = os.path.basename(self.template_file)
     if file_basename.endswith('.j2'):
       file_basename = file_basename.rstrip('.j2')
-    f = open(os.path.join(HOKUSAI_TMP_DIR, file_basename), 'w')
+    f = NamedTemporaryFile(delete=False, dir=HOKUSAI_TMP_DIR)
     self.tmp_filename = f.name
     f.write(self.to_string())
     f.close()
