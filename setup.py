889cac4fd5091c25db303606cef626dbc4e64165
Isac Petruzzi <isac.petruzzi@gmail.com>
2021-05-24 16:05:08 +0200

    fix: update prompt-toolkit so we can install ipdb / ipython in dev mode

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(name='hokusai',
       install_requires=[
           'click~=6.7',
           'click-repl~=0.1',
-          'prompt-toolkit~=1.0.15',
+          'prompt-toolkit~=3.0.18',
           'MarkupSafe~=1.0',
           'Jinja2~=2.10',
           'packaging',
