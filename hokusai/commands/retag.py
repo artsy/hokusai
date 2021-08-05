2d6e797e054690eccb91466ca4932f1af1460068
Jian <jian-feng@artsymail.com>
2021-08-05 11:42:21 -0400

    retag command prints error in red

--- hokusai/commands/retag.py
+++ hokusai/commands/retag.py
@@ -1,5 +1,5 @@
 from hokusai.lib.command import command
-from hokusai.lib.common import print_green, print_yellow
+from hokusai.lib.common import print_green, print_red
 from hokusai.services.ecr import ECR, ClientError
 
 @command()
@@ -13,4 +13,4 @@ def retag(tag_to_change, tag_to_match):
     ecr.retag(tag_to_match, tag_to_change)
     print_green("Updated ECR '%s' tag to point to the image that '%s' tag points to." % (tag_to_change, tag_to_match), newline_after=True)
   except (ValueError, ClientError) as e:
-    print_yellow("WARNING: Updating ECR tag failed due to the error: '%s'" % str(e), newline_before=True, newline_after=True)
+    print_red("WARNING: Updating ECR tag failed due to the error: '%s'" % str(e), newline_before=True, newline_after=True)
