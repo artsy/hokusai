ad1e3d3ebcdc91690568d8468a933ad59e63185f
Jian <jian-feng@artsymail.com>
2022-12-02 17:33:54 -0500

    fix: unused imports

--- hokusai/commands/render.py
+++ hokusai/commands/render.py
@@ -1,36 +1,18 @@
 import os
-import sys
-import time
-import tempfile
-
-from shutil import rmtree, copyfile
-from distutils.dir_util import mkpath
-
-import yaml
-
-from jinja2 import Environment, PackageLoader, FileSystemLoader, StrictUndefined
-
-from hokusai import CWD
-from hokusai.lib.command import command
-from hokusai.lib.config import HOKUSAI_CONFIG_DIR, config
-from hokusai.services.ecr import ECR
-from hokusai.lib.common import print_green, clean_string, shout
-from hokusai.lib.environment import templates_dir_path
-from hokusai.lib.exceptions import HokusaiError
-
-from hokusai.lib.template_selector import TemplateSelector
-
-from hokusai.services.yaml_spec import YamlSpec
-
+from shutil import copytree
 from tempfile import TemporaryDirectory
 
-import shutil
+from hokusai import CWD
+from hokusai.lib.config import HOKUSAI_CONFIG_DIR
+from hokusai.lib.common import shout
+from hokusai.lib.template_selector import TemplateSelector
+from hokusai.services.yaml_spec import YamlSpec
 
 def render(context):
   source_dir = os.path.join(CWD, HOKUSAI_CONFIG_DIR, context)
   # replicate context of source dir into a temp dir
   tempdir = TemporaryDirectory().name
-  shutil.copytree(source_dir, tempdir)
+  copytree(source_dir, tempdir)
   # list out all template files in tempdir
   yamls = TemplateSelector().get_all(tempdir)
   for yaml in yamls:
