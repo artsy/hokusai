5feacead18ae700e7f646acbe219546d94cdc474
Jian <jian-feng@artsymail.com>
2023-02-24 14:37:36 -0500

    feat: add print tree function

--- hokusai/commands/command_tree.py
+++ hokusai/commands/command_tree.py
@@ -1,10 +1,23 @@
 import click
+import pdb
 
 def print_command_tree(click_command):
   """Print a tree of Click commands rooted at the given click_command"""
   tree = {}
   tree.update(build_tree(click_command))
-  print(tree)
+  print_tree(tree)
+
+def print_tree(tree):
+  """Print Click command tree"""
+  for cmdname, subtree in tree.items():
+    if isinstance(tree[cmdname]['command'], click.core.Group):
+      print_tree(tree[cmdname]['children'])
+    else:
+      print_command(tree[cmdname]['command'])
+
+def print_command(click_command):
+  """Print Click command name and its docstring"""
+  print(f'{click_command.name} - {click_command.__doc__}')
 
 def build_tree(click_command):
   """
@@ -15,10 +28,13 @@ def build_tree(click_command):
   """
   cmdname = click_command.name
   tree = {}
+  tree[cmdname] = {}
   if isinstance(click_command, click.core.Group):
-    tree[cmdname] = {}
+    tree[cmdname]['command'] = click_command
+    tree[cmdname]['children'] = {}
     for cmd in click_command.commands.values():
-      tree[cmdname].update(build_tree(cmd))
+      tree[cmdname]['children'].update(build_tree(cmd))
   else:
-    tree[cmdname] = click_command
+    tree[cmdname]['command'] = click_command
+    tree[cmdname]['children'] = {}
   return tree
