7c7aeebed9fda7e47cf6a2f25e10ccc0ea4ee942
Jian <jian-feng@artsymail.com>
2023-03-24 23:26:37 -0400

    feat: break up that monstrous push command

--- hokusai/commands/push.py
+++ hokusai/commands/push.py
@@ -9,37 +9,41 @@ from hokusai.lib.exceptions import HokusaiError
 from hokusai.services.docker import Docker
 
 @command()
-def push(tag, local_tag, build, filename, force, overwrite, skip_latest=False):
-  if not force:
-    git_unclean_files = shout('git status --porcelain --ignored')
-    if git_unclean_files:
-      print_red("The following files/directories are either changed, untracked, or ignored.")
-      print_red("If they are not excluded by .dockerignore, they may get copied into the resulting Docker image.")
-      print(git_unclean_files)
-      raise HokusaiError("Aborting. Re-run command with --force flag, if you are not concerned.")
+def build_and_push(remote_tag, local_tag, filename, ecr, skip_latest=False):
+  Docker().build(filename)
+  push(remote_tag, local_tag, ecr, skip_latest)
 
+def ecr_check():
   ecr = ECR()
   if not ecr.project_repo_exists():
     raise HokusaiError("ECR repo %s does not exist... did you run `hokusai setup` for this project?" % config.project_name)
+  return ecr
 
+@command()
+def git_status_check():
+  git_unclean_files = shout('git status --porcelain --ignored')
+  if git_unclean_files:
+    print_red("The following files/directories are either changed, untracked, or ignored.")
+    print_red("If they are not excluded by .dockerignore, they may get copied into the resulting Docker image.")
+    print(git_unclean_files)
+    raise HokusaiError("Aborting. Re-run command with --force flag, if you are confident.")
+
+@command()
+def push(remote_tag, local_tag, ecr, skip_latest=False):
   shout(ecr.get_login(), mask=(r'^(docker login -u) .+ (-p) .+ (.+)$', r'\1 ****** \2 ***** \3'))
-  if tag is None:
-    tag = shout('git rev-parse HEAD').strip()
+  docker_compose_repo = "hokusai_" + config.project_name
+  tag_and_push(docker_compose_repo, local_tag, ecr.project_repo, remote_tag)
+  if not skip_latest:
+    tag_and_push(docker_compose_repo, local_tag, ecr.project_repo, 'latest')
 
-  if overwrite is None and ecr.tag_exists(tag):
-    raise HokusaiError("Tag %s already exists in registry.  Aborting." % tag)
+def remote_tag_check(remote_tag, overwrite, ecr):
+  if remote_tag is None:
+    remote_tag = shout('git rev-parse HEAD').strip()
+  if not overwrite and ecr.tag_exists(remote_tag):
+    raise HokusaiError("Tag %s already exists in registry.  Aborting." % remote_tag)
+  return remote_tag
 
-  if build:
-    Docker().build()
-
-  build_tag = "hokusai_%s:%s" % (config.project_name, local_tag)
-
-  shout("docker tag %s %s:%s" % (build_tag, ecr.project_repo, tag))
-  shout("docker push %s:%s" % (ecr.project_repo, tag), print_output=True)
-  print_green("Pushed %s to %s:%s" % (build_tag, ecr.project_repo, tag), newline_after=True)
-
-  if skip_latest: return
-
-  shout("docker tag %s %s:%s" % (build_tag, ecr.project_repo, 'latest'))
-  shout("docker push %s:%s" % (ecr.project_repo, 'latest'), print_output=True)
-  print_green("Pushed %s to %s:%s" % (build_tag, ecr.project_repo, 'latest'), newline_after=True)
+def tag_and_push(local_repo, local_tag, remote_repo, remote_tag):
+  shout("docker tag %s:%s %s:%s" % (local_repo, local_tag, remote_repo, remote_tag))
+  shout("docker push %s:%s" % (remote_repo, remote_tag), print_output=True)
+  print_green("Pushed %s:%s to %s:%s" % (local_repo, local_tag, remote_repo, remote_tag), newline_after=True)
