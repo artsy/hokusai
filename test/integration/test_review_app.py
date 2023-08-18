9b4407347319af312c9c35950cd810e8411d7241
Jian <jian-feng@artsymail.com>
2023-08-18 16:39:03 -0400

    fix: review app yml created in source code dir causing coverage to error

--- test/integration/test_review_app.py
+++ test/integration/test_review_app.py
@@ -1,14 +1,15 @@
-import os
 import httpretty
+import os
 import sys
+
 from unittest.mock import patch
 
 from test import HokusaiIntegrationTestCase, TEST_KUBE_CONTEXT
 from test.utils import captured_output
-from test.mocks.mock_ecr import MockECR
+
+import hokusai.commands.namespace
 
 from hokusai import CWD
-from hokusai.lib.common import shout
 from hokusai.commands import kubernetes
 from hokusai.commands.namespace import create_new_app_yaml
 from hokusai.lib.common import shout
@@ -17,10 +18,10 @@ from hokusai.services.kubectl import Kubectl
 class TestReviewApp(HokusaiIntegrationTestCase):
     @classmethod
     def setUpClass(cls):
-        kubernetes.ECR = MockECR
-        kubernetes.HOKUSAI_CONFIG_DIR = os.path.join(CWD, 'test/fixtures/project/hokusai')
-        cls.kubernetes_yml = os.path.abspath(os.path.join(kubernetes.HOKUSAI_CONFIG_DIR, 'a-review-app.yml'))
         cls.kctl = Kubectl(TEST_KUBE_CONTEXT)
+        config_dir = 'test/fixtures/project/hokusai'
+        cls.HOKUSAI_CONFIG_DIR = config_dir
+        hokusai.commands.namespace.HOKUSAI_CONFIG_DIR = config_dir
 
     @classmethod
     def tearDownClass(cls):
@@ -37,9 +38,10 @@ class TestReviewApp(HokusaiIntegrationTestCase):
                                 body=self.fixture('ecr-repositories-response.json'),
                                 content_type="application/x-amz-json-1.1")
 
-        review_app_yaml_file = os.path.join(CWD, 'hokusai', 'a-review-app.yml')
+        review_app_yaml_file = os.path.join(CWD, self.__class__.HOKUSAI_CONFIG_DIR, 'a-review-app.yml')
         try:
-            create_new_app_yaml(os.path.abspath(os.path.join(CWD, 'test/fixtures/project/hokusai/minikube.yml')), 'a-review-app')
+            create_new_app_yaml(os.path.join(CWD, self.__class__.HOKUSAI_CONFIG_DIR, 'minikube.yml'), 'a-review-app')
+            mocked_sys_exit.assert_called_once_with(0)
             self.assertTrue(os.path.isfile(review_app_yaml_file))
         finally:
             os.remove(review_app_yaml_file)
@@ -54,10 +56,10 @@ class TestReviewApp(HokusaiIntegrationTestCase):
                                 body=self.fixture('ecr-repositories-response.json'),
                                 content_type="application/x-amz-json-1.1")
 
-        review_app_yaml_file = os.path.join(CWD, 'hokusai', 'a-review-app.yml')
+        review_app_yaml_file = os.path.join(CWD, self.__class__.HOKUSAI_CONFIG_DIR, 'a-review-app.yml')
         with captured_output() as (out, err):
           try:
-            create_new_app_yaml(os.path.abspath(os.path.join(CWD, 'test/fixtures/project/hokusai/minikube.yml')), 'a-review-app')
+            create_new_app_yaml(os.path.join(CWD, self.__class__.HOKUSAI_CONFIG_DIR, 'minikube.yml'), 'a-review-app')
             mocked_sys_exit.assert_called_once_with(0)
             mocked_sys_exit.reset_mock()
             kubernetes.k8s_create(TEST_KUBE_CONTEXT, filename=review_app_yaml_file)
