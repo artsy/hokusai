af6e154a221de8cb6b06fcf611dcb8323992383b
Jian <jian-feng@artsymail.com>
2023-08-25 19:39:51 -0400

    feat: configure.py writes global config, rather than read it

--- hokusai/commands/configure.py
+++ hokusai/commands/configure.py
@@ -10,26 +10,19 @@ from distutils.dir_util import mkpath
 
 import boto3
 
-from hokusai.lib.global_config import global_config
 from hokusai.lib.command import command
 from hokusai.lib.common import print_green, get_region_name
 from hokusai.lib.exceptions import HokusaiError
 
+from hokusai.lib.constants import YAML_HEADER
+from hokusai.lib.common import print_red
+import yaml
+
+
 @command(config_check=False)
 def configure(kubectl_version, bucket_name, key_name, config_file, install_to, install_config_to):
-  if global_config.is_present() and global_config.kubectl_version is not None:
-    kubectl_version = global_config.kubectl_version
-
   if not kubectl_version:
     raise HokusaiError("You must supply a kubectl_version")
-
-  if global_config.is_present() and global_config.kubectl_config_file is not None:
-    uri = urlparse(global_config.kubectl_config_file)
-    if uri.scheme == 's3':
-      bucket_name = uri.netloc
-      key_name = uri.path
-    if uri.scheme == 'file':
-      key_name = uri.path
 
   if not ((bucket_name and key_name) or config_file):
     raise HokusaiError("You must define valid config_file")
@@ -50,3 +43,16 @@ def configure(kubectl_version, bucket_name, key_name, config_file, install_to, i
     client.download_file(bucket_name, key_name.lstrip('/'), os.path.join(install_config_to, 'config'))
   else:
     shutil.copy(config_file, os.path.join(install_config_to, 'config'))
+
+  write_config_file(install_to)
+
+def write_config_file(install_to):
+  HOKUSAI_GLOBAL_CONFIG_FILE = os.path.join(os.environ.get('HOME', '/'), '.hokusai', 'config.yml')
+  obj = {'bin-dir': install_to}
+  try:
+    with open(HOKUSAI_GLOBAL_CONFIG_FILE, 'w') as output:
+      output.write(YAML_HEADER)
+      yaml.safe_dump(obj, output, default_flow_style=False)
+  except:
+    print_red(f'Error: Not able to write global configuration file {HOKUSAI_GLOBAL_CONFIG_FILE}')
+    raise
