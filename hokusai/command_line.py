9065ab45459a8afc951ad05dab68a81129ecfdbc
Jian <jian-feng@artsymail.com>
2023-09-14 17:09:42 -0400

    fix: calling cert_file_path twice

--- hokusai/command_line.py
+++ hokusai/command_line.py
@@ -4,8 +4,9 @@ import os
 from hokusai.cli import *
 from hokusai.lib.environment import cert_file_path
 
-if cert_file_path() is not None:
-  os.environ['SSL_CERT_FILE'] = cert_file_path()
+path = cert_file_path()
+if path is not None:
+  os.environ['SSL_CERT_FILE'] = path
 
 def main():
   base(obj={})
