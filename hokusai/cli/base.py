964316b122b7aaf07675cb863965466b68e1ee4c
Jian <jian-feng@artsymail.com>
2023-09-14 14:00:48 -0400

    fix: platform option superfluous, because platform is determined dynamically

--- hokusai/cli/base.py
+++ hokusai/cli/base.py
@@ -27,12 +27,11 @@ def console():
 @click.option('--org-config-path', type=click.STRING, default=f"file:///{os.path.join(os.environ.get('HOME'), '.hokusai_org.conf').lstrip('/')}", help='Path to org-wide Hokusai config. Can be local file (e.g. file:///path/to/file) or AWS S3 location (e.g. s3://bucket/prefix/to/file). (default: ~/.hokusai_org.conf)')
 @click.option('--kubectl-path', type=click.STRING, default=os.path.join(os.environ.get('HOME'), '.local', 'bin'), help='Path to install kubectl to. (default: ~/.local/bin/kubectl)')
 @click.option('--kubeconfig-path', type=click.STRING, default=os.path.join(os.environ.get('HOME'), '.kube'), help='Path to download kubeconfig to. (default: ~/.kube/config)')
-@click.option('--platform', type=click.Choice(['darwin', 'linux']), default='darwin', help="Machine's platform.")
 @click.option('-v', '--verbose', type=click.BOOL, is_flag=True, help='Verbose output.')
-def configure(org_config_path, kubectl_path, kubeconfig_path, platform, verbose):
+def configure(org_config_path, kubectl_path, kubeconfig_path, verbose):
   """Fetch org-wide Hokusai config, install kubectl, download kubeconfig, create and save user Hokusai config"""
   set_verbosity(verbose)
-  hokusai.configure(org_config_path, kubectl_path, kubeconfig_path, platform)
+  hokusai.configure(org_config_path, kubectl_path, kubeconfig_path)
 
 
 @base.command(context_settings=CONTEXT_SETTINGS)
--- hokusai/commands/configure.py
+++ hokusai/commands/configure.py
@@ -18,6 +18,9 @@ from hokusai.lib.exceptions import HokusaiError
 
 import pdb
 
+def get_platform():
+  return platform.system().lower()
+
 def download_org_config_from_s3(bucket_name, key_name, file_path):
   client = boto3.client('s3', region_name=get_region_name())
   client.download_file(bucket_name, key_name.lstrip('/'), file_path)
@@ -59,11 +62,11 @@ def read_org_config(org_config_path):
   validate_org_config(org_config)
   return org_config
 
-def create_user_config(org_config, kubectl_path, kubeconfig_path, platform):
+def create_user_config(org_config, kubectl_path, kubeconfig_path):
   user_config = org_config
   user_config['kubectl_path'] = kubectl_path
   user_config['kubeconfig_path'] = kubeconfig_path
-  user_config['platform'] = platform
+  user_config['platform'] = get_platform()
   return user_config
 
 def save_user_config(user_config):
@@ -76,13 +79,13 @@ def save_user_config(user_config):
     print_red(f'Error: Not able to write user Hokusai configuration file {CONFIG_FILE}')
     raise
 
-def install_kubectl(kubectl_version, platform, kubectl_path):
+def install_kubectl(kubectl_version, kubectl_path):
   print_green("Downloading and installing kubectl...", newline_before=True, newline_after=True)
   tmpdir = tempfile.mkdtemp()
   urlretrieve(
     f"https://storage.googleapis.com/kubernetes-release/release/v" +
     f"{kubectl_version}" +
-    f"/bin/{platform.system().lower()}/amd64/kubectl" +
+    f"/bin/{get_platform()}/amd64/kubectl" +
     os.path.join(tmpdir, 'kubectl')
   )
   os.chmod(os.path.join(tmpdir, 'kubectl'), 0o755)
@@ -100,19 +103,17 @@ def install_kubeconfig(kubeconfig_path, bucket_name, key_name):
   client.download_file(bucket_name, key_name.lstrip('/'), os.path.join(kubeconfig_path, 'config'))
 
 @command(config_check=False)
-def configure(org_config_path, kubectl_path, kubeconfig_path, platform):
+def configure(org_config_path, kubectl_path, kubeconfig_path):
   org_config = read_org_config(org_config_path)
 
   user_config = create_user_config(
     org_config,
     kubectl_path,
     kubeconfig_path,
-    platform
   )
 
   install_kubectl(
     user_config['kubectl_version'],
-    user_config['platform'],
     user_config['kubectl_path']
   )
 
