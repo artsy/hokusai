52c7a2ffdf96dc619cd64a2144aa3479e4ec6c20
Jian <jian-feng@artsymail.com>
2023-09-22 15:16:07 -0400

    fix: global_config self.config exposed to clients

--- hokusai/lib/global_config.py
+++ hokusai/lib/global_config.py
@@ -12,21 +12,21 @@ HOKUSAI_GLOBAL_CONFIG_URI = f'file://{HOKUSAI_GLOBAL_CONFIG_FILE}'
 
 class HokusaiGlobalConfig:
   def __init__(self, uri=HOKUSAI_GLOBAL_CONFIG_URI):
-    self.config = ConfigLoader(uri).load()
+    self._config = ConfigLoader(uri).load()
     self.validate_config()
 
   def merge(self, **kwargs):
     ''' merge params into config '''
     for k,v in kwargs.items():
       if v is not None:
-        self.config[k] = v
+        self._config[k] = v
 
   def save(self):
     ''' save config to local config file '''
     try:
       with open(HOKUSAI_GLOBAL_CONFIG_FILE, 'w') as output:
         output.write(YAML_HEADER)
-        yaml.safe_dump(self.config, output, default_flow_style=False)
+        yaml.safe_dump(self._config, output, default_flow_style=False)
     except:
       print_red(f'Error: Not able to write Hokusai config to {file_path}')
       raise
@@ -40,21 +40,21 @@ class HokusaiGlobalConfig:
       'kubectl-dir'
     ]
     for var in required_vars:
-      if not var in self.config:
+      if not var in self._config:
         raise HokusaiError(f'{var} is missing in Hokusai global config')
 
   @property
   def kubeconfig_dir(self):
-    return self.config['kubeconfig-dir']
+    return self._config['kubeconfig-dir']
 
   @property
   def kubeconfig_source_uri(self):
-    return self.config['kubeconfig-source-uri']
+    return self._config['kubeconfig-source-uri']
 
   @property
   def kubectl_dir(self):
-    return self.config['kubectl-dir']
+    return self._config['kubectl-dir']
 
   @property
   def kubectl_version(self):
-    return self.config['kubectl-version']
+    return self._config['kubectl-version']
