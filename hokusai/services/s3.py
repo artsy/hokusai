2faf902bec3f5b8efb26e7c0b72d7fea9f18070a
Jian <jian-feng@artsymail.com>
2023-09-22 15:48:14 -0400

    fix: s3 client visible to class users

--- hokusai/services/s3.py
+++ hokusai/services/s3.py
@@ -11,7 +11,7 @@ from hokusai.services.aws import get_region_name
 class S3Interface:
   ''' interface with AWS S3 '''
   def __init__(self):
-    self.client = boto3.client('s3', region_name=get_region_name())
+    self._client = boto3.client('s3', region_name=get_region_name())
 
   def download(self, uri, target_file):
     ''' download s3://bucket/foo/bar to target_file '''
@@ -21,7 +21,7 @@ class S3Interface:
     if common.get_verbosity():
       common.print_green(f'Downloading {uri} to {target_file} ...', newline_after=True)
     try:
-      self.client.download_file(bucket_name, key_name, target_file)
+      self._client.download_file(bucket_name, key_name, target_file)
     except:
       common.print_red(f'Error: Failed to download {uri} to {target_file}')
       raise
