ce06edd2621547a5a4b4ac95cfb3acb994011205
Jian <jian-feng@artsymail.com>
2023-12-29 10:06:00 -0500

    feat: update test for command wrapper

--- test/unit/test_command_wrapper.py
+++ test/unit/test_command_wrapper.py
@@ -3,9 +3,8 @@ from unittest.mock import patch
 from test import HokusaiUnitTestCase
 from test.utils import captured_output
 
-from hokusai.lib.command import command
+from hokusai.lib.command_wrapper import wrap
 
-@command()
 def foo_command(foo):
   if foo:
     return 0
@@ -13,14 +12,14 @@ def foo_command(foo):
     raise ValueError('Bad command')
 
 class TestCommand(HokusaiUnitTestCase):
-  @patch('hokusai.lib.command.sys.exit', return_code=True)
+  @patch('hokusai.lib.command_wrapper.sys.exit', return_code=True)
   def test_command_exits_with_return_code(self, mocked_sys_exit):
-    foo_command('Ohai!')
+    wrap(foo_command, 'Ohai!')
     mocked_sys_exit.assert_called_once_with(0)
 
-  @patch('hokusai.lib.command.sys.exit', return_code=True)
+  @patch('hokusai.lib.command_wrapper.sys.exit', return_code=True)
   def test_command_catches_exceptions(self, mocked_sys_exit):
     with captured_output() as (out, err):
-      foo_command(False)
+      wrap(foo_command, False)
       mocked_sys_exit.assert_called_once_with(1)
       self.assertIn('ERROR: Bad command', out.getvalue().strip())
