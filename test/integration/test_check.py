09875c1affe794fbea8051150d880506cd45afe8
Jian <jian-feng@artsymail.com>
2024-01-05 17:02:13 -0500

    feat: integration test for hokusai check

--- test/integration/test_check.py
+++ test/integration/test_check.py
@@ -2,6 +2,9 @@ import os
 
 from git import Repo
 
+from click.testing import CliRunner
+from hokusai.cli.base import base
+
 
 # tests operate on a test git repo
 # repo is cloned in conftest.py
@@ -14,3 +17,8 @@ def describe_git_repo_for_test():
     repo = Repo(os.getcwd())
     heads = repo.heads
     assert 'main' in heads
+
+def describe_check():
+  runner = CliRunner()
+  result = runner.invoke(base, ['check'])
+  assert result.exit_code == 0
