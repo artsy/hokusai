cd1604bcd76b8f481e33339db2be1bbdc1ed6b77
Jian <jian-feng@artsymail.com>
2024-01-08 11:56:01 -0500

    feat: clone git repo for integration tests only if dir not present

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -10,7 +10,9 @@ TEST_GIT_REPO_NAME = 'hokusai-sandbox'
 @pytest.fixture(scope="session", autouse=True)
 def clone_repo():
   os.chdir(INTEGRATION_FIXTURES_DIR)
-  git.Git(".").clone(f"https://github.com/artsy/{TEST_GIT_REPO_NAME}.git")
+  # avoid cloning repeatedly when on local
+  if not os.path.isdir(TEST_GIT_REPO_NAME):
+    git.Git(".").clone(f"https://github.com/artsy/{TEST_GIT_REPO_NAME}.git")
   os.chdir(TEST_GIT_REPO_NAME)
 
 @pytest.fixture(autouse=True)
