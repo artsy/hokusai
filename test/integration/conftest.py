ff7a9a7fd576b5d44639e4a81d8afab65539533e
Jian <jian-feng@artsymail.com>
2024-01-16 19:04:24 -0500

    feat: context sanity check to cover circleci 192.168 ips

--- test/integration/conftest.py
+++ test/integration/conftest.py
@@ -1,43 +1,50 @@
 import os
+import re
 import subprocess
 
 import git
 import pytest
 
+from hokusai.lib.common import ansi_escape
+
 
 TEST_GIT_REPO_NAME = 'hokusai-sandbox'
 
-# this function is called before any test runs
+
+def exit_pytest_if_not_minikube(context):
+  ''' exit pytest if context is likely not minikube '''
+  resp = subprocess.run(
+    f'kubectl --context {context} cluster-info',
+    capture_output=True,
+    shell=True,
+    text=True,
+    timeout=5
+  )
+  p = re.compile(r'Kubernetes control plane is running at https://(.*)')
+  m = p.search(ansi_escape(resp.stdout))
+  control_plane_address = m.groups()[0]
+  # 127.0.0.1 = local
+  # 192.168.x.y = circleci
+  if not (
+    control_plane_address.startswith('127.0.0.1') or
+    control_plane_address.startswith('192.168')
+  ):
+    msg = f'Error: {context} context possibly points to a real cluster. Is it a Minikube instance?'
+    pytest.exit(msg)
+
 def pytest_configure(config):
-  # sanity check that 'staging' and 'production' kubernetes contexts point to minikube,
-  # not to real Staging and Production clusters.
-  resp = subprocess.run(
-    'kubectl --context staging cluster-info',
-    capture_output=True,
-    shell=True,
-    text=True,
-    timeout=5
-  )
-  if not 'https://127.0.0.1:' in resp.stdout:
-    msg = 'Error: Staging Kubernetes context does not point to 127.0.0.1. Is staging Minikube setup?'
-    pytest.exit(msg)
-
-  resp = subprocess.run(
-    'kubectl --context production cluster-info',
-    capture_output=True,
-    shell=True,
-    text=True,
-    timeout=5
-  )
-  if not 'https://127.0.0.1:' in resp.stdout:
-    msg = 'Error: Production Kubernetes context does not point to 127.0.0.1. Is production Minikube setup?'
-    pytest.exit(msg)
-
+  ''' do things before tests '''
+  # make sure to not operate on real Staging and Production clusters
+  exit_pytest_if_not_minikube('staging')
+  exit_pytest_if_not_minikube('production')
   # clone test git repo
   os.chdir('test/integration/fixtures')
   # avoid cloning repeatedly when on local
   if not os.path.isdir(TEST_GIT_REPO_NAME):
     git.Git(".").clone(f"https://github.com/artsy/{TEST_GIT_REPO_NAME}.git")
+
+
+## autouse fixtures
 
 @pytest.fixture(scope="session", autouse=True)
 def cd_into_test_git_repo():
