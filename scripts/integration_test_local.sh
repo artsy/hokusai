df8e5b785a3308689b3c1162245b9828a705bfe3
Jian <jian-feng@artsymail.com>
2024-02-01 12:33:07 -0500

    feat: local integration test script to handle regcred

--- scripts/integration_test_local.sh
+++ scripts/integration_test_local.sh
@@ -1,13 +1,30 @@
-# run integration tests locally
+### run integration tests locally
+
+
+## requirements
+
+# - awscli
+# - coverage
+# - docker
+# - jq
+# - kubectl
+# - minikube
+# - pytest
+
+
+## prepare env
 
 export K8S_VERSION=v1.21.14
 export KUBE_CONFIG_PATH=${HOME}/.kube/config
 export KUBE_CONFIG_BACKUP_PATH=${HOME}/.kube/config.bak
-export BACKED_UP_KUBE_CONFIG=false
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 export AWS_REGION=$(aws configure get region)
+export DOCKER_CONFIG_JSON_PATH=${HOME}/.docker/config.json
 
-function start_minikube(){
+
+## functions
+
+function start_minikube() {
   profile=$1
   minikube delete --profile "$profile"
   minikube start --profile "$profile" --kubernetes-version="$K8S_VERSION"
@@ -17,42 +34,82 @@ function start_minikube(){
   done
 }
 
-if ls "$KUBE_CONFIG_PATH"
-then
-  echo "Backing up $KUBE_CONFIG_PATH ..."
-  mv "$KUBE_CONFIG_PATH" "$KUBE_CONFIG_BACKUP_PATH"
-  export BACKED_UP_KUBE_CONFIG=true
-fi
+function create_real_docker_config_json() {
+  # on Mac, Docker config.json points to credsStore
+  # where the creds are really stored,
+  # create a file that has the creds inline
+  local dockercfg=$(mktemp ~/.docker/config.json.XXX)
+  local storetype=$(jq -r .credsStore < $DOCKER_CONFIG_JSON_PATH)
+  local registry=''
+  (
+    echo '{'
+    echo '    "auths": {'
+    for registry in $(docker-credential-$storetype list | jq -r 'to_entries[] | .key'); do
+      if [ ! -z "$FIRST" ]; then
+          echo '        },'
+      fi
+      FIRST='true'
+      credential=$(echo $registry | docker-credential-$storetype get | jq -jr '"\(.Username):\(.Secret)"' | base64)
+      echo '        "'$registry'": {'
+      echo '            "auth": "'$credential'"'
+    done
+    echo '        }'
+    echo '    }'
+    echo '}'
+  ) > $dockercfg
+  echo "$dockercfg"
+}
 
-start_minikube staging
-start_minikube production
+function back_up_kube_config() {
+  # don't let minikube overwrite user's real k8s kubeconfig
+  local backed_up_kube_config=''
+  if ls "$KUBE_CONFIG_PATH"
+  then
+    echo "Backing up $KUBE_CONFIG_PATH ..."
+    mv "$KUBE_CONFIG_PATH" "$KUBE_CONFIG_BACKUP_PATH"
+    backed_up_kube_config='true'
+  fi
+  echo "$backed_up_kube_config"
+}
 
-# create ecr image pull secret in minikube/k8s
-
-aws ecr get-login-password --region "$AWS_REGION" | \
-  docker login --username AWS \
-    --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
-
-kubectl --context staging create secret generic regcred \
-  --from-file=.dockerconfigjson=${HOME}/.docker/config.json \
-  --type=kubernetes.io/dockerconfigjson
-
-kubectl --context staging patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
-
-kubectl --context production create secret generic regcred \
-  --from-file=.dockerconfigjson=${HOME}/.docker/config.json \
-  --type=kubernetes.io/dockerconfigjson
-
-kubectl --context production patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
-
-
-coverage run -m pytest test/integration
-
-minikube stop --profile staging
-minikube stop --profile production
-
-if [ "$BACKED_UP_KUBE_CONFIG" = "true" ]
-then
+function restore_kube_config() {
   echo "Restoring $KUBE_CONFIG_PATH ..."
   mv "$KUBE_CONFIG_BACKUP_PATH" "$KUBE_CONFIG_PATH"
+}
+
+function create_regcred() {
+  # create ecr image pull secret in minikube/k8s
+  aws ecr get-login-password --region "$AWS_REGION" | \
+    docker login --username AWS \
+      --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+  local dockercfg=$(create_real_docker_config_json)
+  # ensure file is deleted when script exits
+  trap "rm -f $dockercfg" EXIT
+  create_regcred_for_env 'staging' $dockercfg
+  create_regcred_for_env 'production' $dockercfg
+  rm -f "$dockercfg"
+}
+
+function create_regcred_for_env() {
+  local environment=$1
+  local dockercfg=$2
+  kubectl --context "$environment" create secret generic regcred \
+    --from-file=.dockerconfigjson="$dockercfg" \
+    --type=kubernetes.io/dockerconfigjson
+  kubectl --context "$environment" patch serviceaccount default -p '{"imagePullSecrets": [{"name": "regcred"}]}'
+}
+
+
+## main
+
+kube_config_is_backed_up=$(back_up_kube_config)
+start_minikube staging
+start_minikube production
+create_regcred
+coverage run -m pytest test/integration
+minikube stop --profile staging
+minikube stop --profile production
+if [ "$kube_config_is_backed_up" = "true" ]
+then
+  restore_kube_config
 fi
