afc70a3c1467dd9d49771b94db92b52b49f96f95
Jian <jian-feng@artsymail.com>
2024-05-24 09:46:50 -0700

    feat: let spec compare against literal string

--- test/unit/test_services/fixtures/yaml_spec.py
+++ test/unit/test_services/fixtures/yaml_spec.py
@@ -2,7 +2,4 @@ import pytest
 
 @pytest.fixture
 def test_k8s_spec_as_string():
-  test_k8s_spec = 'test/fixtures/kubernetes-config.yml'
-  with open(test_k8s_spec, 'r') as f:
-    content = f.read().strip()
-  return content
+  return "---\napiVersion: extensions/v1beta1\nkind: Deployment\nmetadata:\n  name: {{ project_name }}\n  namespace: default\nspec:\n  strategy:\n    rollingUpdate:\n      maxSurge: 1\n      maxUnavailable: 0\n    type: RollingUpdate\n  template:\n{% include 'deployment-metadata.yml' %}\n    spec:\n      containers:\n        {%- for container in vars.containers %}\n        - name: {{ container.name }}\n          image: {{ vars.imageTag }}\n        {%- endfor %}"
