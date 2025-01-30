73afd5225e5b057a5efec817b89c33516bef2c19
Jian <jian-feng@artsymail.com>
2025-01-30 11:45:08 -0800

    turn repeat code in development module into lib helpers

--- hokusai/commands/development.py
+++ hokusai/commands/development.py
@@ -1,107 +1,83 @@
 import os
 import signal
 
-from hokusai import CWD
-from hokusai.lib.config import HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE, config
+from hokusai.lib.config import HOKUSAI_CONFIG_DIR, config
 from hokusai.lib.common import print_green, shout, EXIT_SIGNALS
-from hokusai.lib.exceptions import HokusaiError
 from hokusai.services.docker import Docker
-from hokusai.lib.template_selector import TemplateSelector
-from hokusai.lib.docker_compose_helpers import follow_extends
-from hokusai.services.yaml_spec import YamlSpec
+from hokusai.lib.docker_compose_helpers import generate_compose_command, get_yaml_template
+
 
 def dev_start(build, detach, filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
+  compose_command = generate_compose_command(filename)
   def cleanup(*args):
-    shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai stop" % docker_compose_yml, print_output=True)
+    shout(
+      f'{compose_command} -p hokusai stop',
+      print_output=True
+    )
   for sig in EXIT_SIGNALS:
     signal.signal(sig, cleanup)
-
   opts = ''
   if build:
+    yaml_template = get_yaml_template(filename)
     Docker().build(filename=yaml_template)
   if detach:
     opts += ' -d'
-
   if not detach:
     print_green("Starting development environment... Press Ctrl+C to stop.")
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai up%s" % (docker_compose_yml, opts), print_output=True)
-
+  shout(
+    f'{compose_command} -p hokusai up{opts}',
+    print_output=True
+  )
   if detach:
     print_green("Run `hokousai dev stop` to shut down, or `hokusai dev logs --follow` to tail output.")
 
 def dev_stop(filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai stop" % docker_compose_yml, print_output=True)
+  compose_command = generate_compose_command(filename)
+  shout(
+    f'{compose_command} -p hokusai stop',
+    print_output=True
+  )
 
 def dev_status(filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai ps" % docker_compose_yml, print_output=True)
+  compose_command = generate_compose_command(filename)
+  shout(
+    f'{compose_command} -p hokusai ps',
+    print_output=True
+  )
 
 def dev_logs(follow, tail, filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
+  compose_command = generate_compose_command(filename)
   opts = ''
   if follow:
     opts += ' --follow'
   if tail:
     opts += " --tail=%i" % tail
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai logs%s" % (docker_compose_yml, opts), print_output=True)
+  shout(
+    f'{compose_command} -p hokusai logs{opts}',
+    print_output=True
+  )
 
 def dev_run(container_command, service_name, stop, filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
+  compose_command = generate_compose_command(filename)
   if service_name is None:
     service_name = config.project_name
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai run %s %s" % (docker_compose_yml, service_name, container_command), print_output=True)
-
+  shout(
+    f'{compose_command} -p hokusai run {service_name} {container_command}',
+    print_output=True
+  )
   if stop:
-    shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai stop" % docker_compose_yml, print_output=True)
+    shout(
+      f'{compose_command} -p hokusai stop',
+      print_output=True
+    )
 
 def dev_clean(filename):
-  if filename is None:
-    yaml_template = TemplateSelector().get(os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE))
-  else:
-    yaml_template = TemplateSelector().get(filename)
-
-  docker_compose_yml = YamlSpec(yaml_template).to_file()
-  follow_extends(docker_compose_yml)
-
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai stop" % docker_compose_yml, print_output=True)
-  shout("COMPOSE_COMPATIBILITY=true docker-compose -f %s -p hokusai rm --force" % docker_compose_yml, print_output=True)
+  compose_command = generate_compose_command(filename)
+  shout(
+    f'{compose_command} -p hokusai stop',
+    print_output=True
+  )
+  shout(
+    f'{compose_command} -p hokusai rm --force',
+    print_output=True
+  )
--- hokusai/lib/docker_compose_helpers.py
+++ hokusai/lib/docker_compose_helpers.py
@@ -3,10 +3,11 @@ import os
 import yaml
 
 from hokusai import CWD
-from hokusai.lib.config import HOKUSAI_CONFIG_DIR
-from hokusai.lib.exceptions import HokusaiError
+from hokusai.lib.config import HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE
 from hokusai.lib.template_selector import TemplateSelector
+from hokusai.services.docker import Docker
 from hokusai.services.yaml_spec import YamlSpec
+
 
 def follow_extends(docker_compose_yml):
   with open(docker_compose_yml, 'r') as f:
@@ -22,3 +23,29 @@ def follow_extends(docker_compose_yml):
       extended_template = TemplateSelector().get(extended_template_path)
       rendered_templates.append(YamlSpec(extended_template).to_file())
     return rendered_templates
+
+def generate_compose_command(filename):
+  ''' return Docker Compose command '''
+  docker_compose_yml = render_docker_compose_yml(filename)
+  return (
+    'COMPOSE_COMPATIBILITY=true ' +
+    Docker.compose_command() +
+    f' -f {docker_compose_yml}'
+  )
+
+def get_yaml_template(filename):
+  ''' return yaml template '''
+  if filename is None:
+    yaml_template = TemplateSelector().get(
+      os.path.join(CWD, HOKUSAI_CONFIG_DIR, DEVELOPMENT_YML_FILE)
+    )
+  else:
+    yaml_template = TemplateSelector().get(filename)
+  return yaml_template
+
+def render_docker_compose_yml(filename):
+  ''' return path of rendered Docker Compose yaml '''
+  yaml_template = get_yaml_template(filename)
+  docker_compose_yml = YamlSpec(yaml_template).to_file()
+  follow_extends(docker_compose_yml)
+  return docker_compose_yml
